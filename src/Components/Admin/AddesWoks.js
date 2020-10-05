c0a41145f7a29a2ee2d87dcad4b0d1c0eb861915
asifhasan24 <asifmehu24@gmail.com>
2020-10-05 22:23:50 +0600

    done

--- src/Components/Admin/AddesWoks.js
+++ src/Components/Admin/AddesWoks.js
@@ -11,7 +11,7 @@ const handle=()=>{
     fetch('http://localhost:7000/addWorks',{
         method:'POST',
         headers:{
-            'Content-Type': 'application/json'
+            'Content-Type':'application/json'
         },
         body:JSON.stringify(fakeData)
     })
